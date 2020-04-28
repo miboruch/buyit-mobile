e3bb8094bd7f75bab16a6d8abcec49c21ca9e0f8
dawidplesniarski <dawidplesniarski@gmail.com>
2020-04-28 23:52:44 +0200

    Photo added to main screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Screen from './Screen';
 import Button from '../components/Button';
 
@@ -7,8 +7,11 @@ const HomeScreen = ({ navigation }) => {
   return (
     <Screen navigation={navigation} theme='dark'>
       <View style={styles.container}>
+        <Text style={styles.title}>OUR PERSONAL</Text>
+        <Text style={styles.title}>SHOPPING EXPERIENCE</Text>
+        <Image style={styles.image} source={require('../assets/images/main_logo.jpg')}></Image>
         <Text style={styles.text}>Hello from Home Screen</Text>
-        <Button
+        <Button style = {styles.button}
           text={'Go to products'}
           onPress={() =>
             navigation.navigate('ProductScreen', {
@@ -19,6 +22,19 @@ const HomeScreen = ({ navigation }) => {
         >
           Go to products
         </Button>
+        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
+
+        <Button
+            text={'Account'}
+        >
+        </Button>
       </View>
     </Screen>
   );
@@ -27,12 +43,24 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center'
+  },
+  title: {
+    fontFamily: 'Futura',
+    fontSize: 20,
+    color: '#f1f1f1'
   },
   text: {
     fontFamily: 'Futura',
+    fontSize: 20,
     color: '#f1f1f1'
+  },
+  image: {
+    width: '100%',
+    height: '35%'
+  },
+  button: {
+    justifyContent: 'center'
   }
 });
 
