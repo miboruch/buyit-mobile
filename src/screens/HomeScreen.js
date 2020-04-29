12e9483dc36a607eb5793f035cfde1baa19b6945
dawidplesniarski <dawidplesniarski@gmail.com>
2020-04-29 13:07:45 +0200

    SmallButton component added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Screen from './Screen';
 import Button from '../components/Button';
+import SmallButton from "../components/SmallButton";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <Screen navigation={navigation} theme='dark'>
       <View style={styles.container}>
+        <View style={styles.smallbutton}>
+          <SmallButton />
+          <SmallButton />
+          <SmallButton />
+        </View>
         <Text style={styles.title}>OUR PERSONAL</Text>
         <Text style={styles.title}>SHOPPING EXPERIENCE</Text>
-        <Image style={styles.image} source={require('../assets/images/main_logo.jpg')}></Image>
-        <Text style={styles.text}>Hello from Home Screen</Text>
+        <Text style={styles.title}>EXPERIENCE</Text>
+        <Image style={styles.image} source={require('../assets/images/main_logo.jpg')}/>
         <Button style = {styles.button}
           text={'Go to products'}
           onPress={() =>
@@ -18,20 +24,12 @@ const HomeScreen = ({ navigation }) => {
               category: 'electronics'
             })
           }
-          isButtonDark={false}
-        >
-          Go to products
-        </Button>
-        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
-        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
-        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
-        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
-        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
-        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
-        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
-        <Text style={styles.text}> Lorem ipsum dolor sit amet</Text>
-
+          isButtonDark={false}/>
+        <Text style={styles.text}> Look for products, you can order without an account</Text>
+        <Text style={styles.text}> You must have an account to add product to store</Text>
+        <Text style={styles.text}> If you click reserve product will be reserved for you for 15 minutes</Text>
         <Button
+            style={styles.button}
             text={'Account'}
         >
         </Button>
@@ -46,21 +44,33 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   title: {
+    alignSelf: 'flex-start',
     fontFamily: 'Futura',
-    fontSize: 20,
-    color: '#f1f1f1'
+    padding:10,
+    fontSize: 25,
+    color: '#868990',
   },
   text: {
     fontFamily: 'Futura',
-    fontSize: 20,
-    color: '#f1f1f1'
+    fontSize: 18,
+    color: '#c0c5ce',
+    alignSelf: 'flex-start',
+    padding: 10,
   },
   image: {
     width: '100%',
-    height: '35%'
+    height: '35%',
+    padding:5
   },
   button: {
-    justifyContent: 'center'
+    flex:1,
+    alignSelf: 'center',
+    paddingTop: 10
+  },
+  smallbutton: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent:'space-around'
   }
 });
 
