4119694b8ac04760088c7696dabf35ac5f334f37
dawidplesniarski <dawidplesniarski@gmail.com>
2020-05-18 15:09:06 +0200

    CartScreen.js added

---
+++ src/screens/CartScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View } from "react-native";
+
+
+const CartScreen = ({ route, navigation }) => {
+    return (
+        <Screen navigation={navigation} theme={'light'}>
+            <View style={styles.container}>
+
+            </View>
+        </Screen>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+
+export default CartScreen;
